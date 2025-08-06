bb0b858fddeddbf7b2531f3b2abd36afc8e33409
antonsafonov1999 <antonsafonov1999@gmail.com>
2025-08-06 15:52:13 +0200

    add Project Students
    add Student Class
    add Menu Structur
    Add StudentManager Struct

---
+++ Student/Student/ManagerMenu.swift
@@ -0,0 +1,72 @@
+//
+//  ManagerMenu.swift
+//  Student
+//
+//  Created by Anton Safonov on 06/08/2025.
+//
+import Darwin
+struct ManagerMenu
+{
+    static func ClearConsole(){print("\u{001B}[2J\u{001B}[H")}
+    
+    static private func showMenu()
+    {
+        print("""
+            \\\\\\\\\\\\MENU\\\\\\\\\\\\
+            1) ДОБАВЛЕНИЕ СТУДЕНТА🧑‍🎓
+            2) ДОБАВЛЕНИЕ ПРЕДМЕТА📚
+            3) ДОБАВЛЕНИЕ ОЦЕНКИ СТУДЕНТУ ПО ПРЕДМЕТУ💯
+            4) ПРОСМОТР ИНФОРМАЦИИ ПО СТУДЕНТУℹ️
+            5) ВЫВОД СРЕДНЕГО БАЛА ПО СТУДЕНТУ ％
+            6) ВЫВОД СРЕДНЕГО БАЛА ВСЕХ СТУДЕНТОВ％
+            7) ВЫВОД ЛУЧШИХ СТУДЕНТОВ🥇
+            8) ВЫВОД ХУДШИХ СТУДЕНТОВ🥉
+            9) СОХРАНИТЬ В jsone📝
+            0) ВЫХОД ❌
+            """)
+    }
+    
+    static public func OpenMenu()
+    {
+        print("Показать меню? (y/n)\n")
+        if readLine() == "y"{choiceMenu()}else{print("Меню не показано")}
+    }
+    static private func choiceMenu()
+    {
+        showMenu( )
+        print("Выдите номер пункта меню:")
+        
+        while true
+        {
+            if let num = readLine(), let choice = Int(num)
+            {
+                switch choice
+                {
+                case 0:
+                    exit(0)
+                case 1:
+                    StudentManager.addStudent()
+                case 2:
+                    StudentManager.addSubject()
+                case 3:
+                    StudentManager.addMark()
+                case 4:
+                    StudentManager.showStudentInfo()
+                case 5:
+                    StudentManager.showStudentAverage()
+                case 6:
+                    StudentManager.showAllStudentsAverage()
+                case 7:
+                    StudentManager.showBestStudents()
+                case 8:
+                    StudentManager.showWorstStudents()
+                case 9:
+                    StudentManager.saveToJSON()
+                default:
+                    print("Такого пункта нет. Попробуйте снова"); continue
+                }
+                break
+            }else{print("Неверный ввод, введи цифру"); }
+        }
+    }
+}
---
+++ Student/Student/StudentManager.swift
@@ -0,0 +1,46 @@
+//
+//  StudentManager.swift
+//  Student
+//
+//  Created by Anton Safonov on 06/08/2025.
+//
+
+struct StudentManager
+{
+    static func addStudent() {
+            print("Добавляем студента...")
+            // твой код добавления студента
+        }
+        static func addSubject() {
+            print("Добавляем предмет...")
+            // твой код добавления предмета
+        }
+        static func addMark() {
+            print("Добавляем оценку студенту...")
+            // твой код добавления оценки
+        }
+        static func showStudentInfo() {
+            print("Показываем информацию по студенту...")
+            // твой код показа информации
+        }
+        static func showStudentAverage() {
+            print("Средний балл студента...")
+            // твой код подсчёта среднего балла
+        }
+        static func showAllStudentsAverage() {
+            print("Средний балл всех студентов...")
+            // твой код подсчёта среднего по всем
+        }
+    static func showBestStudents() {
+           print("Лучшие студенты...")
+           // твой код показа лучших
+       }
+       static func showWorstStudents() {
+           print("Худшие студенты...")
+           // твой код показа худших
+       }
+       static func saveToJSON() {
+           print("Сохраняем в JSON...")
+           // твой код сохранения в JSON
+       }
+}
