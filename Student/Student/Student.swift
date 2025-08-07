e8a389d4c5379c93c9ffff40c20fd887f67fbf8c
antonsafonov1999 <antonsafonov1999@gmail.com>
2025-08-07 20:02:55 +0200

    work with func
    addStudentWithNameAndSurname

--- Student/Student/Student.swift
+++ Student/Student/Student.swift
@@ -11,8 +11,9 @@ class Student
     var lastName: String = ""
     var object: String = ""
     var rating: Double = 0.0
+    let noneObject = "NONE"
     
-    var StudentsMass: [Student] = []
+//    var StudentsMass: [Student] = []
     
     enum AddErrorStudent: Error
     {
@@ -23,21 +24,7 @@ class Student
 
         case TooBigSmallFirstName
         case TooBigSmallLastName
-        case TooBigSmolObject
-        
-    }
-    deinit{print(print("🗑️ Студент \(self.firstName) \(self.lastName) удалён из памяти"))}
-    
-    init?()
-    {
-        print("Данный метод создании обьекта , создает пустой обьект, с базовыми значениями.\nВы хотите продолжить?(y/n) ")
-        if readLine() != "y" { print("обьект не создан");return nil }else{
-            firstName = "firstName"
-            lastName = "lastName"
-            object = "object"
-            rating = 0.0
-            print("Обьект создан")
-        }
+        case TooBigSmallObject
         
     }
     func validateFirstLastName(_ firstName: String, _ lastname: String) throws -> Bool
@@ -48,13 +35,33 @@ class Student
         if firstName.count < 3 || firstName.count > 10 { throw AddErrorStudent.TooBigSmallFirstName}
         return true
     }
-    func validateObjectrating(_ object: String, _ rating: Double)throws -> Bool
+    func validateRating(_ rating: Double)throws -> Bool
     {
-        if object.isEmpty { throw AddErrorStudent.emptyObject}
-        if object.count < 3 || object.count > 10 { throw AddErrorStudent.TooBigSmolObject}
         if rating < 0 || rating > 100 { throw AddErrorStudent.RangeNegativeRating}
         return true
     }
+    func validateObject(_ object: String) throws  -> Bool
+    {
+        if object.isEmpty { throw AddErrorStudent.emptyObject}
+        if object.count < 3 || object.count > 10 { throw AddErrorStudent.TooBigSmallObject}
+        return true
+    }
+    
+    deinit{print(print("🗑️ Студент \(self.firstName) \(self.lastName) удалён из памяти"))}
+    
+    init?()
+    {
+        print("Данный метод создании обьекта , создает пустой обьект, с базовыми значениями.\nВы хотите продолжить?(y/n) ")
+        if readLine() != "y" { print("обьект не создан");return nil }else{
+            firstName = "noneFirstName"
+            lastName = "noneLastName"
+            object = "NONE"
+            rating = 0.0
+            print("Обьект создан")
+        }
+        
+    }
+   
     init?(firstName: String, lastname: String)
     {
         do {
@@ -62,6 +69,8 @@ class Student
             {
                  self.firstName = firstName
                  self.lastName = lastname
+                 self.object = noneObject
+                 self.rating = 0.0
                  return
              }else {print("Ошибка в инициализации обьекта!\nОшибка при создании Имени или Фамилии!\n"); return nil}
         }catch AddErrorStudent.emptyFirstName
@@ -85,32 +94,33 @@ class Student
             return nil
         }
     }
-    init?(firstName: String, lastName: String, object: String, rating: Double)
+    
+    convenience init?(firstName: String, lastName: String, object: String)
     {
         do
         {
-            let a = try validateFirstLastName(firstName, lastName)
-            let b = try validateObjectrating(object, rating)
-            if a && b
+            self.init(firstName: firstName, lastname: lastName)
+            let validationObject = try validateObject(object)
+            if validationObject
             {
-                self.firstName = firstName
-                self.lastName = lastName
                 self.object = object
-                self.rating = rating
+                self.rating = 0.0
                 return
             }else {print("Ошибка в инициализации обьекта!\nОшибка при создании предмета или рейтинга!\n"); return nil}
-        }catch AddErrorStudent.RangeNegativeRating
-        {
-            print("Ошибка при инициализации обьекта, \"РЕЙТИНГ\" не может быть меньше 0 или больше 100!\nОбьект не будет создан!\n")
-            return nil
-        }catch AddErrorStudent.emptyObject
-        {
-            print("Ошибка при инициализации обьекта, \"ПРЕДМЕТ\"  не может быть путсым! \nОбьект не будет создан!\n")
-            return nil
-        } catch
-        {
-            print("Неизвестаня ошибка! При инициализации премета или оценки обьекта возникла ошибка\nОбьект не будет создан!\n")
-            return nil
-        }
+                }catch AddErrorStudent.RangeNegativeRating
+                {
+                    print("Ошибка при инициализации обьекта, \"РЕЙТИНГ\" не может быть меньше 0 или больше 100!\nОбьект не будет создан!\n")
+                    return nil
+                }catch AddErrorStudent.emptyObject
+                {
+                    print("Ошибка при инициализации обьекта, \"ПРЕДМЕТ\"  не может быть путсым! \nОбьект не будет создан!\n")
+                    return nil
+                } catch
+                {
+                    print("Неизвестаня ошибка! При инициализации премета или оценки обьекта возникла ошибка\nОбьект не будет создан!\n")
+                    return nil
+                }
+        
     }
+
 }
--- Student/Student/StudentManager.swift
+++ Student/Student/StudentManager.swift
@@ -7,9 +7,65 @@
 
 struct StudentManager
 {
+    static var students: [Student] = []
+    static func addStudentWithNameAndSurname()
+    {
+        while true
+        {
+            print(#"Введите имя студента:"#)
+            guard let name = readLine() else { continue}
+            print(#"Введите фамилию студента:"#)
+            guard let surname = readLine() else { continue}
+            if let student = Student(firstName: name, lastname: surname)
+            {
+                students.append(student)
+                print("Студент \(name) \(surname) добавлен!\n")
+            }else{
+                print("Ошибка: Не удалось создать студента.\n")
+            }
+           
+            print("Продолжить добавление студентов? (y/n)\n")
+            if readLine() == "n"
+            {
+                
+                break
+            }else { continue }
+        }
+        print("\\\\\\\\\\Массив студентов\\\\\\\\\\\n")
+        for i in students
+        {
+            print("Name: \(i.firstName) Surname: \(i.lastName)\nObject: \(i.object), Rating: \(i.rating)\n")
+        }
+        
+    }
+    static func addStudentWithNameAndSurnameAndSubject()
+    {
+        
+    }
+    static func addStudentWithNameAndSurnameAndSubjectAndMark()
+    {
+        
+    }
     static func addStudent() {
-            print("Добавляем студента...")
-            // твой код добавления студента
+            print("Добавляем студента...\nВАРИАНТЫ:\n1) Студент с Именем и фамилией\n2) Студент с Именем, фамилией, предметом\n3) Студент с Именем, фамилией, предметом и оценкой\nВыдите номер пункта меню:\n")
+        while true
+            {
+                if let choice = readLine(), let choiceInt = Int(choice)
+            {
+                switch choiceInt
+                {
+                case 1:
+                    addStudentWithNameAndSurname()
+                case 2:
+                    addStudentWithNameAndSurnameAndSubject()
+                case 3:
+                    addStudentWithNameAndSurnameAndSubjectAndMark()
+                default:
+                    print("Такого пункта нет. Попробуйте снова"); continue
+                }
+                    break
+                }else{print("Неверный ввод, введи цифру"); }
+            }
         }
         static func addSubject() {
             print("Добавляем предмет...")
